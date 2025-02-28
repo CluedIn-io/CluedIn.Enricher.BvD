4e45acd514a1283bb1707e77fbffb23a603472f6
Yi-Wei <ywei390@gmail.com>
2025-02-28 19:18:13 +0800

    chore: Remove unnecessary filtering on bvdids

--- src/ExternalSearch.Providers.BvD/BvDExternalSearchProvider.cs
+++ src/ExternalSearch.Providers.BvD/BvDExternalSearchProvider.cs
@@ -164,17 +164,10 @@ namespace CluedIn.ExternalSearch.Providers.BvD
                     foreach (var value in filteredValues)
                     {
                         request.CustomQueryInput = bvdId.ElementAt(0);
-                        var cleaner = new BvDNumberCleaner();
-                        var sanitizedValue = cleaner.CheckBvDNumber(value);
+                        
+                        context.Log.LogInformation("External search query produced, ExternalSearchQueryParameter: '{Identifier}' EntityType: '{EntityCode}' Value: '{SanitizedValue}'", ExternalSearchQueryParameter.Identifier, entityType.Code, value);
 
-                        if (value != sanitizedValue)
-                        {
-                            context.Log.LogTrace("Sanitized VAT number. Original '{OriginalValue}', Updated '{SanitizedValue}'", value, sanitizedValue);
-                        }
-
-                        context.Log.LogInformation("External search query produced, ExternalSearchQueryParameter: '{Identifier}' EntityType: '{EntityCode}' Value: '{SanitizedValue}'", ExternalSearchQueryParameter.Identifier, entityType.Code, sanitizedValue);
-
-                        yield return new ExternalSearchQuery(this, entityType, ExternalSearchQueryParameter.Identifier, sanitizedValue);
+                        yield return new ExternalSearchQuery(this, entityType, ExternalSearchQueryParameter.Identifier, value);
                     }
                 }
 
