dce91ec2e5e823364e9dee3107d3f39948e3a6d1
Yi-Wei <86753236+ywei390@users.noreply.github.com>
2025-10-29 17:00:12 +0800

    fix: Resolve AddVocabularyKeyAsync object reference is null issue (#44)

--- src/ExternalSearch.Providers.BvD/BvDExternalSearchProvider.cs
+++ src/ExternalSearch.Providers.BvD/BvDExternalSearchProvider.cs
@@ -1052,7 +1052,7 @@ public class BvDExternalSearchProvider : ExternalSearchProviderBase, IExtendedEn
                     IsVisible = true,
                     Storage = VocabularyKeyStorage.Keyword
                 };
-                var vocabKeyId = vocabularyRepository.AddVocabularyKeyAsync(context, newVocabKey).GetAwaiter().GetResult();
+                var vocabKeyId = vocabularyRepository.AddVocabularyKeyAsync(context, newVocabKey, context.Organization.Id, Guid.Empty).GetAwaiter().GetResult();
                 vocabularyRepository.ActivateVocabularyKeyAsync(context, vocabKeyId).GetAwaiter().GetResult();
             }
             context.ApplicationContext.System.Cache.SetItem(cacheKey, new object(), DateTimeOffset.Now.AddMinutes(1));
